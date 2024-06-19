494cf2242cdfd64f1d117b68423b628932a2582a
Atul Tripathi <atul55tripathi@gmail.com>
2024-06-19 19:39:21 +0530

    javascript

---
+++ learningpages/javascript/javabase.js
@@ -0,0 +1,33 @@
+// take input ans show output
+// var x=5;
+//  naam= prompt("what is your name?");
+//  alert ("your name is " + naam);
+
+//count number of words in the string
+// var s=prompt("enter the string:");
+// var k=s.length;
+// alert("the number of words are: " + k);
+
+//input a string ans output only first 10 letters of the string, similar to s.substr(from, kitna);
+// s = prompt("enter the string(only first 10 will be taken");
+// t= s.slice(0,10);
+// alert(t);
+
+// take name, and alert the name by making the first letter capital and others small
+// naam=prompt("enter your name");
+// alert("Your name is : " + naam.slice(0,1).toUpperCase() + naam.slice(1,naam.length).toLowerCase() );
+
+// function to convert 
+// function cal(n)
+// {
+//     console.log(Math.floor(n/1.5));
+// }
+// cal(5);
+
+// function to print how many weeks and months for given number of days
+// function weekmonth(k)
+// {
+//     console.log("number of weeks: "+ Math.floor(k/7)+ " and number of months: " + Math.floor(k/30));
+// }
+// k=prompt("number of days");
+// weekmonth(k);
