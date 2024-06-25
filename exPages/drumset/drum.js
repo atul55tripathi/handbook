611e293800e62c7d2162db35b7f81de2e8a63290
Atul Tripathi <atul55tripathi@gmail.com>
2024-06-26 02:20:02 +0530

    pta nhi

--- exPages/drumset/drum.js
+++ exPages/drumset/drum.js
@@ -4,9 +4,10 @@
 //     }
 
 //or
+var aud=new Audio('./sounds/crash.mp3');
 var n=document.querySelectorAll(".drum").length
 for(i=0;i<n;i++)
-    document.querySelectorAll("button")[i].addEventListener("click",handleClick);
-function handleClick(){
-    alert("i got clicked");
-}
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        console.log(this.classList[0]);
+        this.style.color="white";
+    });
