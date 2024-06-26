574a516a6a981243a9fd4c4165b798302f1a6d59
Atul Tripathi <atul55tripathi@gmail.com>
2024-06-26 16:05:48 +0530

    drum khatam

--- exPages/drumset/drum.js
+++ exPages/drumset/drum.js
@@ -4,10 +4,46 @@
 //     }
 
 //or
-var aud=new Audio('./sounds/crash.mp3');
+
+
 var n=document.querySelectorAll(".drum").length
 for(i=0;i<n;i++)
     document.querySelectorAll("button")[i].addEventListener("click",function(){
-        console.log(this.classList[0]);
-        this.style.color="white";
+        var c=this.classList[0];
+        keytomusic(c);
     });
+
+document.addEventListener('keypress',function(event)
+{
+    var c=event.key;
+    keytomusic(c);
+});
+
+function keytomusic(c){
+    switch(c)
+        {
+            case 'w':
+            var aud=new Audio('./sounds/crash.mp3');
+            aud.play();
+            break;
+            case 'a':
+            var aud=new Audio('./sounds/kick-bass.mp3');
+            aud.play();
+            break;case 's':
+            var aud=new Audio('./sounds/snare.mp3');
+            aud.play();
+            break;case 'd':
+            var aud=new Audio('./sounds/tom-1.mp3');
+            aud.play();
+            break;case 'j':
+            var aud=new Audio('./sounds/tom-2.mp3');
+            aud.play();
+            break;case 'k':
+            var aud=new Audio('./sounds/tom-3.mp3');
+            aud.play();
+            break;case 'l':
+            var aud=new Audio('./sounds/tom-4.mp3');
+            aud.play();
+            break;
+        }
+}
--- learningpages/javascript/eventing.js
+++ learningpages/javascript/eventing.js
@@ -3,3 +3,17 @@ clicker.addEventListener("click",clickHandler);
 function clickHandler(){
     clicker.style.color="red";
 }
+function doggy(name,age,owner)
+{
+    this.name=name;
+    this.age=age;
+    this.owner=owner;
+    this.bark=function(){
+        alert("bow bow");
+    }
+}
+
+var orry=new doggy("kalu",2,"peelu");
+document.addEventListener('keypress',function(event){
+    console.log(event.key);
+})
